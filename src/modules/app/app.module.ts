702561602892f6ddab2a597910bcd4a2881486a6
codenoobies <kevindubidaam@gmail.com>
2024-03-05 14:04:56 +0700

    del: Deleted app.controller.ts & app.service.ts

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
 import {PublicModule} from '../public/public.module'
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 
 @Module({
   imports: [PublicModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
