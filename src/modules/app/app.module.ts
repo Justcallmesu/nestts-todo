7026a2eaa51a2e3b241083367fd80e74d3e87f6e
codenoobies <kevindubidaam@gmail.com>
2024-03-05 13:57:49 +0700

    Connected Public Module to the app main module

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import {PublicModule} from '../public/public.module'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [PublicModule],
   controllers: [AppController],
   providers: [AppService],
 })
