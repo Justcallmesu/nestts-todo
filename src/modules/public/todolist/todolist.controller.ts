31fb680e12f6f67827680909ccfdb017a05905f6
codenoobies <kevindubidaam@gmail.com>
2024-03-05 14:25:46 +0700

    feat: Created TodoListDTO template

--- src/modules/public/todolist/todolist.controller.ts
+++ src/modules/public/todolist/todolist.controller.ts
@@ -1,11 +1,6 @@
 import { Controller,Req,Param,Get,Post } from "@nestjs/common";
 import { Request } from "express";
 
-function printData(value:Function){
-    console.log(value);
-}
-
-@printData
 @Controller("todo")
 export class TodoController{
     @Get("/:id")
